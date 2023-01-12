19a8a5d514c65f2f63ec28b07ada2bd8a49ac5b5
mpongkorn <104343838+mpongkorn@users.noreply.github.com>
2023-01-12 15:20:55 +0700

    Update CAN_send-receive.ino

--- CANBUS-BMS/CAN_send-receive.ino
+++ CANBUS-BMS/CAN_send-receive.ino
@@ -1,3 +1,4 @@
+
 // CAN Send Example
 //
 
@@ -17,55 +18,98 @@ void setup()
   Serial.begin(115200);
 
   // Initialize MCP2515 running at 16MHz with a baudrate of 500kb/s and the masks and filters disabled.
-  if(CAN0.begin(MCP_ANY, CAN_125KBPS, MCP_16MHZ) == CAN_OK)
+  if (CAN0.begin(MCP_ANY, CAN_500KBPS, MCP_16MHZ) == CAN_OK)
     Serial.println("MCP2515 Initialized Successfully!");
   else Serial.println("Error Initializing MCP2515...");
 
   CAN0.setMode(MCP_NORMAL);   // Change to normal mode to allow messages to be transmitted
 
   pinMode(CAN0_INT, INPUT);                            // Configuring pin for /INT input
-  
+
   Serial.println("MCP2515 Library Receive Example...");
 }
 
-byte data[8] = {0x01, 0x0E, 0x44, 0x0E, 0x43, 0x0E, 0x42, 0x00};
+//byte data[8] = {0x01, 0x0E, 0x44, 0x0E, 0x43, 0x0E, 0x42, 0x00};
+byte data[8] = {};
+//byte can_id[8] = {0x18910140,0x18920140,0x18930140,0x18940140,0x18950140,0x18960140,0x18970140,0x18980140};
 
 void loop()
 {
   // send data:  ID = 0x100, Standard CAN Frame, Data length = 8 bytes, 'data' = array of data bytes to send
+  // Voltage ----------------------------------------------------------------------------------------
   byte sndStat = CAN0.sendMsgBuf(0x18950140, 1, 8, data);
-  if(sndStat == CAN_OK){
-    Serial.println("Message Sent Successfully!");
+  if (sndStat == CAN_OK) {
+    //Serial.println("Message Sent Successfully!");
   } else {
     Serial.println("Error Sending Message...");
   }
   delay(100);   // send data per 100ms
 
   // receive data ---------------------------------------------------------------------
-  if(!digitalRead(CAN0_INT))                         // If CAN0_INT pin is low, read receive buffer
+  if (!digitalRead(CAN0_INT))                        // If CAN0_INT pin is low, read receive buffer
   {
-    CAN0.readMsgBuf(&rxId, &len, rxBuf);      // Read data: len = data length, buf = data byte(s)
-    
-    if((rxId & 0x80000000) == 0x80000000)     // Determine if ID is standard (11 bits) or extended (29 bits)
-      sprintf(msgString, "Extended ID: 0x%.8lX  DLC: %1d  Data:", (rxId & 0x1FFFFFFF), len);
-    else
-      sprintf(msgString, "Standard ID: 0x%.3lX       DLC: %1d  Data:", rxId, len);
-  
-    Serial.print(msgString);
-  
-    if((rxId & 0x40000000) == 0x40000000){    // Determine if message is a remote request frame.
-      sprintf(msgString, " REMOTE REQUEST FRAME");
+    for (int i = 0; i < 2; i++) {
+      CAN0.readMsgBuf(&rxId, &len, rxBuf);      // Read data: len = data length, buf = data byte(s)
+
+      if ((rxId & 0x80000000) == 0x80000000)    // Determine if ID is standard (11 bits) or extended (29 bits)
+        sprintf(msgString, "Extended ID: 0x%.8lX  DLC: %1d  Data:", (rxId & 0x1FFFFFFF), len);
+      else
+        sprintf(msgString, "Standard ID: 0x%.3lX       DLC: %1d  Data:", rxId, len);
+
       Serial.print(msgString);
-    } else {
-      for(byte i = 0; i<len; i++){
-        sprintf(msgString, " %.2X", rxBuf[i]);
+
+      if ((rxId & 0x40000000) == 0x40000000) {  // Determine if message is a remote request frame.
+        sprintf(msgString, " REMOTE REQUEST FRAME");
         Serial.print(msgString);
+      } else {
+        for (byte i = 0; i < len; i++) {
+          sprintf(msgString, " %.2X", rxBuf[i]);
+          Serial.print(msgString);
+        }
       }
+
+      Serial.println();
     }
-        
-    Serial.println();
+  }
+
+  // send data:  ID = 0x100, Standard CAN Frame, Data length = 8 bytes, 'data' = array of data bytes to send
+  // Voltage ----------------------------------------------------------------------------------------
+  byte sndStat1 = CAN0.sendMsgBuf(0x18920140, 1, 8, data);
+  if (sndStat1 == CAN_OK) {
+    //Serial.println("Message Sent Successfully!");
+  } else {
+    Serial.println("Error Sending Message...");
+  }
+  delay(100);   // send data per 100ms
+
+  // receive data ---------------------------------------------------------------------
+  if (!digitalRead(CAN0_INT))                        // If CAN0_INT pin is low, read receive buffer
+  {
+    for (int i = 0; i < 2; i++) {
+      CAN0.readMsgBuf(&rxId, &len, rxBuf);      // Read data: len = data length, buf = data byte(s)
+
+      if ((rxId & 0x80000000) == 0x80000000)    // Determine if ID is standard (11 bits) or extended (29 bits)
+        sprintf(msgString, "Extended ID: 0x%.8lX  DLC: %1d  Data:", (rxId & 0x1FFFFFFF), len);
+      else
+        sprintf(msgString, "Standard ID: 0x%.3lX       DLC: %1d  Data:", rxId, len);
+
+      Serial.print(msgString);
+
+      if ((rxId & 0x40000000) == 0x40000000) {  // Determine if message is a remote request frame.
+        sprintf(msgString, " REMOTE REQUEST FRAME");
+        Serial.print(msgString);
+      } else {
+        for (byte i = 0; i < len; i++) {
+          sprintf(msgString, " %.2X", rxBuf[i]);
+          Serial.print(msgString);
+        }
+      }
+
+      Serial.println();
+    }
   }
 }
+
 
 /*********************************************************************************************************
   END FILE
